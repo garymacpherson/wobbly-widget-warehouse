d016b599021ada783a72e3a8d98bbd975b97ac88
Gary MacPherson <gary.macpherson@team.belong.com.au>
2022-08-05 11:00:54 +0100

    log4shell

--- dockerfile
+++ dockerfile
@@ -1,5 +1,7 @@
 FROM tomcat:9.0.60-jre11-openjdk-slim-buster
 
+ENV SUPER_SECRET_API_KEY=password123!
+
 ADD /build/libs/api.war /usr/local/tomcat/webapps/
 
 CMD ["catalina.sh", "run"]
--- src/main/java/com/wobblywidgetwarehouse/api/OrderController.java
+++ src/main/java/com/wobblywidgetwarehouse/api/OrderController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.ui.Model;
 import com.wobblywidgetwarehouse.api.Contracts.WidgetOrderRequest;
 
 @RestController()
 @RequestMapping("/order")
 public class OrderController {
+    static Logger logger = LogManager.getLogger(OrderController.class);
+
 	@GetMapping("/widget")
 	public String greetingForm(Model model) {
 		model.addAttribute("request", new WidgetOrderRequest());
@@ -20,6 +24,11 @@ public class OrderController {
 	@PostMapping("/widget")
 	public String greetingSubmit(@ModelAttribute WidgetOrderRequest request, Model model) {
 		model.addAttribute("request", request);
+
+		logger.info("new widget order received");
+		logger.info(request.getType());
+		logger.info(request.getSpecialRequest());
+
 		return "widget";
 	}
 }
